7eeb595250c7a0f7e4db07b244fa447d009dbeca
Hoenn <evan.mena13@gmail.com>
2017-04-23 17:41:35 -0400

    Create directories specified by user

--- main.go
+++ main.go
@@ -1,33 +1,36 @@
 package main
 
-import(
-  "bufio"
-  "os"
-  "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
 )
 
-func main(){
-  scanner := bufio.NewScanner(os.Stdin)
-  artistName, albumName := getNames(scanner)
-  //is this Correct prompt?
-  fmt.Printf("\nArist: %v Album: %v\n", artistName, albumName)
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	artistName, albumName := getNames(scanner)
+	//is this Correct prompt?
+	fmt.Printf("\nArtist: %v Album: %v\n", artistName, albumName)
 
-  //If no loop
+	//If no loop
 
-
-  //If yes then...
+	artistPath := filepath.Join(".", artistName)
+	os.MkdirAll(artistPath, os.ModePerm)
+	albumPath := filepath.Join(".", artistName, albumName)
+	os.MkdirAll(albumPath, os.ModePerm)
 }
 
-func getNames (scanner *bufio.Scanner) (string,string) {
+func getNames(scanner *bufio.Scanner) (string, string) {
 
-  fmt.Print("Enter artist name: ")
-  scanner.Scan()
-  artistName := scanner.Text();
+	fmt.Print("Enter artist name: ")
+	scanner.Scan()
+	artistName := scanner.Text()
 
-  fmt.Print("Enter album name: ")
-  scanner.Scan()
-  albumName := scanner.Text();
+	fmt.Print("Enter album name: ")
+	scanner.Scan()
+	albumName := scanner.Text()
 
-  return artistName, albumName
+	return artistName, albumName
 
 }
